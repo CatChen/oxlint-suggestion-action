48a7d14f9aa56e813042aab81985c1156ed8f2ed
Cat Chen <catchen@catchen.me>
2026-03-10 22:07:42 +0000

    Test commit for #138 (55832a24efd5dd905222bcc7c571b8f92802892a) by run 22926488271 (235-1)

---
+++ src/__tests__/for-direction.js
@@ -0,0 +1,12 @@
+/* for-direction: "error" */
+
+for (var i = 0; i < 10; i--) {}
+
+for (var i = 10; i >= 0; i++) {}
+
+for (var i = 0; i > 10; i++) {}
+
+for (var i = 0; 10 > i; i--) {}
+
+const n = -2;
+for (let i = 0; i < 10; i += n) {}
